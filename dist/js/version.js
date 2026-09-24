bc909b199a4a52a4c10f26bfbbf652280fa665c0
benzema172 <purczynskiwiktor@gmail.com>
2026-09-24 08:22:37 +0200

    Deploy complete PureInvest OS 1.9.28 interaction runtime release

--- dist/js/version.js
+++ dist/js/version.js
@@ -1,4 +1,4 @@
 (function(){
-  const release=Object.freeze({version:'1.9.27',build:'2026-09-23-auth-runtime-stability-v1927',name:'PureInvest OS 1.9.27',date:'2026-09-23',schema:'SQL_FINAL_SCHEMA_1_9_0.sql',optionalMigration:'SQL_HOTFIX_ADMIN_WIKBEN17_1_9_14.sql'});
+  const release=Object.freeze({version:'1.9.28',build:'2026-09-24-interaction-kernel-v1928',name:'PureInvest OS 1.9.28',date:'2026-09-24',schema:'SQL_FINAL_SCHEMA_1_9_0.sql',optionalMigration:'SQL_HOTFIX_ADMIN_WIKBEN17_1_9_14.sql'});
   window.PI_RELEASE=release; window.APP_VERSION=release.version; window.APP_BUILD=release.build; window.APP_RELEASE_NAME=release.name;
 })();
